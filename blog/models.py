636c702d427a44e52e7f6087b830903f790342ed
pineapplepen <cdubd10@gmail.com>
2018-02-22 15:02:47 -0800

    spike on rendering blog posts with markdown content

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+
+class Post(models.Model):
+    title   = models.CharField(max_length=200, default='')
+    slug    = models.SlugField(unique=True, default='')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.title)
+
+        super(Post, self).save(*args, **kwargs)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.template import loader
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Post
+
+
+
+def index(request):
+    post = Post.objects.latest('created_at')
+    template = loader.get_template('blog/index.html')
+    context = {
+        'post': post
+    }
+    print(context)
+    return HttpResponse(template.render(context, request)) 
+
+def detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    template = loader.get_template('blog/detail.html')
+    context = {
+        'post': post
+    }
+    print(context)
+    return HttpResponse(template.render(context, request)) 
