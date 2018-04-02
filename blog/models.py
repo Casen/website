2e0dbff705973a4af54f104dd961c4a72ba0c6f9
pineapplepen <cdubd10@gmail.com>
2018-04-02 12:56:15 -0700

    add categories for blog posts

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,24 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Category(models.Model):
+    title   = models.CharField(max_length=200, default='')
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     title   = models.CharField(max_length=200, default='')
     slug    = models.SlugField(unique=True, default='')
-    content = models.TextField()
+    description = models.TextField(default='')
+    content = models.TextField(default='')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
 
     def save(self, *args, **kwargs):
         if not self.id:
