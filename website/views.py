b4a2b089fc8772027657f2f1013896c03535a00e
pineapplepen <cdubd10@gmail.com>
2018-04-05 16:47:31 -0700

    pull the latest blog post for the homepage

--- website/views.py
+++ website/views.py
@@ -3,10 +3,14 @@ from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.http import HttpResponse
 
+from blog.models import Post
+
 
 def index(request):
+    post = Post.objects.latest('created_at')
     return render(request, 'index.html', {
-        'started_coding_date': datetime.datetime(2004, 4, 8)
+        'started_coding_date': datetime.datetime(2004, 4, 8),
+        'post': post
     })
 
 def contact(request):
