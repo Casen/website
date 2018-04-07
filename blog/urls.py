fab9c31dc9c1fff97fae8b2d7855ee08a33e925c
pineapplepen <cdubd10@gmail.com>
2018-04-07 13:05:20 -0700

    clean up url pointers to blog posts

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<slug:slug>/', views.detail, name='detail'),
+    path('', views.index, name='blog_index'),
+    path('<slug:slug>/', views.detail, name='blog_detail'),
 ]
